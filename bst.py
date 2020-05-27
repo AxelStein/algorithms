5644881029d9c92f92df5040d7d30d68d39b0792
DieFaust <diefaust1993@gmail.com>
2020-05-27 17:50:21 +0300

    Fix bst

--- bst.py
+++ bst.py
@@ -21,123 +21,110 @@ class Node:
 class BST:
     def __init__(self):
         self.root = None
-        self.remove_from = None
 
-    def insert(self, root, val):
-        if root is None:
-            self.root = Node(val)
-            return
-        if val == root.val:
-            return
-        if val < root.val:
-            if root.left:
-                self.insert(root.left, val)
-            else:
-                root.left = Node(val)
+    def insert(self, val):
+        self.root = self._insert(self.root, val)
+
+    def _insert(self, node, val):
+        if node is None:
+            return Node(val)
+        if val < node.val:
+            node.left = self._insert(node.left, val)
         else:
-            if root.right:
-                self.insert(root.right, val)
-            else:
-                root.right = Node(val)
+            node.right = self._insert(node.right, val)
+        return node
 
-    def find_node(self, root, val):
+    def delete(self, val):
+        self.root = self._delete(self.root, val)
+
+    def _delete(self, node, val):
+        if node is None:
+            return None
+        if node.val == val:
+            # two children case
+            if node.left and node.right:
+                # find the most left node in the right subtree
+                # swap values and remove leaf node
+                tmp = self._find_min(node.right)
+                node.val = tmp.val
+                node.right = self._delete(node.right, tmp.val)
+            else:
+                return BST._get_child(node)
+
+        if val < node.val:
+            node.left = self._delete(node.left, val)
+        else:
+            node.right = self._delete(node.right, val)
+
+        return node
+
+    def find_node(self, val):
+        return self._find_node(self.root, val)
+
+    def _find_node(self, root, val):
         if root is None:
             return None
         if root.val == val:
             return root
         if val < root.val:
-            return self.find_node(root.left, val)
+            return self._find_node(root.left, val)
         else:
-            return self.find_node(root.right, val)
+            return self._find_node(root.right, val)
 
-    def find_most_left_node(self, root):
+    @staticmethod
+    def _get_child(node):
+        if node:
+            if node.left:
+                return node.left
+            elif node.right:
+                return node.right
+        return None
+
+    def _find_min(self, root):
         if root is None:
             return None
         if not root.left:
             return root
         else:
-            return self.find_most_left_node(root.left)
+            return self._find_min(root.left)
 
-    def remove(self, node, val):
-        if node is None:
-            return
-        if not self.root:
-            return
-        if self.root.val == val:
-            # fixme check children
-            return
-
-        if node.val == val:
-            # two children case
-            if node.left and node.right:
-                # find the most left node in the right subtree
-                s = self.find_most_left_node(node.right)
-                node.val = s.val
-                s.val = -1
-                self.remove(node, -1)
-            # one child case
-            elif node.left or node.right:
-                BST.swap_nodes(self.remove_from, node, BST.get_child(node))
-            # no children case
-            else:
-                BST.swap_nodes(self.remove_from, node, None)
-            self.remove_from = None
-            return
-
-        if node.left:
-            self.remove_from = node
-            self.remove(node.left, val)
-
-        if node.right:
-            self.remove_from = node
-            self.remove(node.right, val)
-
-    @staticmethod
-    def swap_nodes(n1, n2, to):
-        if BST.node_equals(n1.left, n2):
-            n1.left = to
-        elif BST.node_equals(n1.right, n2):
-            n1.right = to
-
-    @staticmethod
-    def get_child(n):
-        if n.left:
-            return n.left
-        if n.right:
-            return n.right
-        return None
-
-    @staticmethod
-    def node_equals(n1, n2):
-        return n1 and n2 and n1.val == n2.val
-
-    def print_tree(self, root):
+    def _print_tree(self, root):
         if root:
             print(root)
-            self.print_tree(root.left)
-            self.print_tree(root.right)
+            self._print_tree(root.left)
+            self._print_tree(root.right)
+
+    def __str__(self):
+        self._print_tree(self.root)
+        return "----"
 
 
 bst = BST()
-bst.insert(bst.root, 5)
-bst.insert(bst.root, 1)
-bst.insert(bst.root, 3)
-bst.insert(bst.root, 4)
-bst.insert(bst.root, 9)
-bst.insert(bst.root, 7)
-bst.insert(bst.root, 6)
-bst.insert(bst.root, 8)
-bst.insert(bst.root, 2)
-bst.insert(bst.root, 10)
+bst.insert(5)
+bst.insert(1)
+bst.insert(3)
+bst.insert(4)
+bst.insert(9)
+bst.insert(7)
+bst.insert(6)
+bst.insert(8)
+bst.insert(2)
+bst.insert(10)
 
-bst.print_tree(bst.root)
-print("----")
+print(bst)
 
-bst.remove(bst.root, 9)
+print("delete 2")
+bst.delete(2)
+print(bst)
 
-print("----")
-bst.print_tree(bst.root)
+print("delete 1")
+bst.delete(1)
+print(bst)
 
-# bst.remove(bst.root, 3)
-# bst.print_tree(bst.root)
-# print("----")
+print("delete 7")
+bst.delete(7)
+print(bst)
+
+print("delete 5")
+bst.delete(5)
+print(bst)
