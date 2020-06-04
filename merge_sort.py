5ee7652a4edf572aa9e02138ad802bd2b6bf6575
Alex <diefaust1993@gmail.com>
2020-06-04 12:35:33 +0300

    Edit merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -6,20 +6,39 @@ def swap(arr, i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
 
-def merge(arr, low, high):
-    print(low, high, int(high / 2))
-    if high - low == 0:
-        return low
-    mid = int(low + (high - low) / 2)
-    r1 = merge(arr, low, mid)
-    r2 = merge(arr, mid + 1, high)
-    print("ret", r1, r2)
-    return r1, r2
+def sort(arr, i, j):
+    print('sort', i, j, arr)
+    start = j
+    end = (j - i) + j
+    n = i
+    v = arr[i]
+    while n < end:
+        if v < arr[j]:
+            arr[n] = v
+            i += 1
+            v = arr[i]
+        else:
+            arr[n] = arr[j]
+            j += 1
+        n += 1
 
 
-def sort():
-    pass
+def merge(arr, start, end):
+    if end - start == 0:
+        return start
+    mid = int(start + (end - start) / 2)
+    l = merge(arr, start, mid)
+    h = merge(arr, mid + 1, end)
+    sort(arr, l, h)
+    return start
 
 
-arr = np.array([6, 5, 3, 1, 8, 7, 2, 4])
-merge(arr, 0, len(arr)-1)
+# 5613 2478
+a = np.array([6, 5, 3, 1, 8, 7, 2, 4])
+# s = np.zeros(len(a), dtype=int)
+# print(s)
+
+merge(a, 0, len(a) - 1)
+print(a)
+
+# print(s)
